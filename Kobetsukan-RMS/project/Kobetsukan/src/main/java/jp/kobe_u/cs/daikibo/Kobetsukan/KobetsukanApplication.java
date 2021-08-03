2af2813e3c0e53547de5811d7b941480a22b8a01
gushiken-ui <cycle.masa@gmail.com>
2021-08-03 09:24:21 +0900

    spring boot

---
+++ Kobetsukan-RMS/project/Kobetsukan/src/main/java/jp/kobe_u/cs/daikibo/Kobetsukan/KobetsukanApplication.java
@@ -0,0 +1,13 @@
+package jp.kobe_u.cs.daikibo.Kobetsukan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KobetsukanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KobetsukanApplication.class, args);
+	}
+
+}
---
+++ Kobetsukan-RMS/project/Kobetsukan/src/test/java/jp/kobe_u/cs/daikibo/Kobetsukan/KobetsukanApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.kobe_u.cs.daikibo.Kobetsukan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KobetsukanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
