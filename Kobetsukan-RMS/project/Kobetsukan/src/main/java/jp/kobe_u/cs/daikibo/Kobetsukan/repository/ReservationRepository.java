8370b3799d496c39ebc16554c2319e8f5c8c8e82
uni-kaz <kazu.unigame@gmail.com>
2021-08-13 21:23:03 +0900

    トップページに予約した講習会が表示されるように

--- Kobetsukan-RMS/project/Kobetsukan/src/main/java/jp/kobe_u/cs/daikibo/Kobetsukan/repository/ReservationRepository.java
+++ Kobetsukan-RMS/project/Kobetsukan/src/main/java/jp/kobe_u/cs/daikibo/Kobetsukan/repository/ReservationRepository.java
@@ -1,6 +1,7 @@
 package jp.kobe_u.cs.daikibo.Kobetsukan.repository;
 
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -18,6 +19,10 @@ public interface ReservationRepository extends CrudRepository<Reservation, Long>
 
     //先生の全予約を取得する
     public Iterable<Reservation> findByteacherId(String teacherId);
+
+    // 生徒の全予約を取得する
+    @Query(value = "SELECT * FROM reservation WHERE student_id1 = ?1 OR student_id2 = ?1", nativeQuery = true)
+    public List<Reservation> findByStudentId(String studentId);
 
     /*
     *与えられた日時に重複している予約の個数を取得する
