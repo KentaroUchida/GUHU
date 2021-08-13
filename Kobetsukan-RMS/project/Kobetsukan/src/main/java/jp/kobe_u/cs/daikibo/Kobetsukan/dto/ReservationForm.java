1df3e1ed51e7746b19791dc81f2d2b88b1eecd61
gushiken-ui <cycle.masa@gmail.com>
2021-08-13 13:09:38 +0900

    reservationform修正

---
+++ Kobetsukan-RMS/project/Kobetsukan/src/main/java/jp/kobe_u/cs/daikibo/Kobetsukan/dto/ReservationForm.java
@@ -0,0 +1,87 @@
+package jp.kobe_u.cs.daikibo.Kobetsukan.dto;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+//import java.util.Date;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import jp.kobe_u.cs.daikibo.Kobetsukan.entity.Reservation;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 予約のフォームクラス
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReservationForm {
+    Long number; // 予約番号 (新規作成時はnull)
+    /**
+     * 日付
+     */
+    @NotBlank
+    @Pattern(regexp = "\\d{4}-\\d{2}-\\d{2}")
+    String date;
+    /**
+     * その日の何コマ目か
+     */
+    @NotNull
+    Integer period;
+    /**
+     * 講師ID
+     */
+    @NotBlank
+    String teacherId;
+    /**
+     * 自分の生徒ID
+     */
+    @NotBlank
+    String studentId1;
+        /**
+     * 自分の生徒ID
+     */
+    @NotBlank
+    String studentId2;
+
+
+    /**
+     * フォームをエンティティに変換する
+     * 
+     * @return 予約エンティティ
+     */
+    public Reservation toEntity() {
+        Reservation r = new Reservation();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+        try {
+            r.getRid();
+            r.setDate(sdf.parse(date));
+            r.getPeriod();
+            r.getTeacherId();
+            r.setStudentId1(studentId1);
+            r.setStudentId2(studentId2);
+
+        } catch (ParseException e) {
+            // 例外処理
+            e.printStackTrace();
+        }
+        return r;
+    }
+
+    /**
+     * エンティティをフォームに変換する
+     * 
+     * @param r 予約エンティティ
+     * @return 予約フォーム
+     */
+    public static ReservationForm toForm(Reservation r) {
+        //SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+        ReservationForm f = new ReservationForm(r.getRid(),r.getDate().toString(),
+                                   r.getPeriod(),r.getTeacherId(),r.getStudentId1(),r.getStudentId2());
+        return f;
+    }
+}
